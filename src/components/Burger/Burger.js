73a787623b096442a2cd3d1e3ad00b465638b13e
Lucas Sônego <sonego_lucas@outlook.com>
2020-01-25 13:30:44 -0200

    feat: dynamic ingredient list on state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,32 +1,13 @@
 import React from "react";
 
 import { BurgerDiv } from "./styles";
-import {
-  BreadTop,
-  Seeds1,
-  Seeds2,
-  Salad,
-  Cheese,
-  Bacon,
-  Meat,
-  BreadBottom
-} from "./BurgerIngredient/BurgerIngredient";
+import { BreadTop, BreadBottom } from "./BurgerIngredient/BurgerIngredient";
 
 export default function Burger(props) {
   return (
     <BurgerDiv>
-      <BreadTop>
-        {props.seeds && (
-          <>
-            <Seeds1 />
-            <Seeds2 />
-          </>
-        )}
-      </BreadTop>
-      {props.salad && <Salad />}
-      {props.cheese && <Cheese />}
-      {props.bacon && <Bacon />}
-      {props.meat && <Meat />}
+      <BreadTop />
+      {props.ingredients.map(ing => ing())}
       <BreadBottom />
     </BurgerDiv>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Burger from "../../components/Burger/Burger";
+import {
+  Bacon,
+  Cheese,
+  Salad,
+  Meat
+} from "../../components/Burger/BurgerIngredient/BurgerIngredient";
 
 export default function BurgerBuilder() {
+  const salad = () => <Salad />;
+  const bacon = () => <Bacon />;
+  const cheese = () => <Cheese />;
+  const meat = () => <Meat />;
+
+  const [ingredients, setIngredients] = useState({
+    list: [bacon, cheese, bacon]
+  });
+
+  function test() {
+    setIngredients({
+      list: [...ingredients.list, salad, cheese, bacon, meat]
+    });
+  }
+
   return (
     <>
-      <Burger
-        salad={false}
-        cheese={true}
-        bacon={true}
-        meat={true}
-        seeds={true}
-      />
-      <div>BuildControls</div>
+      <Burger ingredients={ingredients.list} />
+      <div>
+        BuildControls
+        <button onClick={() => test()}>TEST</button>
+      </div>
     </>
   );
 }
