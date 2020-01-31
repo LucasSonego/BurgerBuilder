e0a5671a8a97dc3ad1ddc9edbe4391dd6786f11c
Lucas Sônego <lucassonego@ufpr.br>
2020-01-31 20:39:15 -0200

    feat: backdrop

---
+++ src/components/UI/Backdrop/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: 100;
+  background: rgba(0, 0, 0, 0.3);
+`;
