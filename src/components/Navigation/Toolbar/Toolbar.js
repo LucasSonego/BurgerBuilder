ae1afb51c0c3564000f97fd588838b6c812fe914
Lucas Sônego <sonego_lucas@outlook.com>
2020-02-11 15:19:22 -0200

    feat: toolbar

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Header } from "./styles";
+
+export default function Toolbar(props) {
+  return (
+    <Header>
+      <div>Menu</div>
+      <div>Logo</div>
+      <div>...</div>
+    </Header>
+  );
+}
