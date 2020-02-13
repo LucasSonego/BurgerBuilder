0c5da80b43b5b087a630201ab268306d39848036
Lucas Sônego <lucassonego@ufpr.br>
2020-02-13 18:32:04 -0200

    feat: SIdeDrawer toggle button and backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Main } from "./styles";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 export default function Layout(props) {
+  const [sideDrawerState, setSideDrawerState] = useState(false);
+
+  function toggleSideDrawer() {
+    setSideDrawerState(!sideDrawerState);
+  }
+
   return (
     <>
-      <Toolbar />
-      <SideDrawer open="true" />
+      <Toolbar toggleSideDrawer={toggleSideDrawer} />
+      <SideDrawer open={sideDrawerState} toggleSideDrawer={toggleSideDrawer} />
       <Main>{props.children}</Main>
     </>
   );
