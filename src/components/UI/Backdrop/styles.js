20083d833910ddf82d96295bf1ad30cfde09cccb
Lucas Sônego <sonego_lucas@outlook.com>
2020-02-14 17:06:23 -0200

    fix: backdrop now covers the toolbar

--- src/components/UI/Backdrop/styles.js
+++ src/components/UI/Backdrop/styles.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 100%;
-  width: 100%;
   position: fixed;
+  height: 200%;
+  width: 200%;
+  top: -50%;
+  left: -50%;
+  overflow: hidden;
   z-index: 100;
   background: rgba(0, 0, 0, 0.3);
 `;
