030456171423d2eb271b88c6c71dadf7d72e9a35
Lucas Sônego <lucassonego@ufpr.br>
2020-02-14 15:39:37 -0200

    refactor: Layout moved to the containers folder

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 import { Main } from "./styles";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 export default function Layout(props) {
   const [sideDrawerState, setSideDrawerState] = useState(false);
